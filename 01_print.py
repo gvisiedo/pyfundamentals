c9f706d8bc7c01c9f2c4a54d70beba340d2c3787
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-12-06 22:26:53 +0100

    .

---
+++ 01_print.py
@@ -0,0 +1,10 @@
+print("Archivo 1")
+# Comentarios de una linea
+#print(12 * 5)
+
+print(10 + 5)
+
+'''
+Comentario de varias lineas
+'''
+
---
+++ 02_vars.py
@@ -0,0 +1,22 @@
+my_name = "Nico"
+
+print("Hola, vars")
+
+print(my_name)
+
+my_age = 12
+
+print(my_age)
+
+
+
+my_name= "Pepe"
+
+print(my_name)
+
+#input
+
+
+my_name = input("¿Cual es tu nombre?")
+
+print("usando input", my_name)
